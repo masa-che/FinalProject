d10cc92312e3d6a8323b319208d5f092c69b0b03
masa-che <masache1983@gmail.com>
2022-09-12 03:24:27 +0300

    add exception and assert

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,7 +1,23 @@
+from selenium.common.exceptions import NoSuchElementException
+
+
 class BasePage():
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, timeout=5):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)       # неявное ожидание
 
     def open(self):
-        self.browser.get(self.url)
+        self.browser.get(self.url)                  # открытие страницы по адресу в url
+
+    def is_element_present(self, how, what):        # функция перехвата исключения (exception)
+        try:                                        # выполняем инструкцию, которая может породить исключение
+            self.browser.find_element(how, what)
+        except NoSuchElementException:              # перехват самого исключения
+            return False
+        return True
+
+# функцию is_element_present будем юзать в main_page (by.css_selector-how, "#селектор"-what)
+# how  - аргумент КАК искать (xpath, id, class, css etc)
+# what - аргумент ЧТО искать (str-selector)
+    
--- test_main_page.py
+++ test_main_page.py
@@ -1,13 +1,16 @@
 from .pages.main_page import MainPage
-from selenium.webdriver.common.by import By
 
 
-def test_guest_can_go_to_login_page(browser):
+def test_guest_can_go_to_login_page(browser):       # тест перехода на страницу
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)                  # инициализируем Page Object, передаём в конструктор экземпляр драйвера и урлу
     page.open()                                     # открываем страницу
-    page.go_to_login_page()                         # выполняем метод страницы - переходим на страницу логина
+    page.go_to_login_page()                         # выполняем метод(def) страницы - переходим на страницу логина
 
 
-
+def test_guest_should_see_login_link(browser):      # проверка перехода по видимой (css-maine_page)  линке
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)                  # для читабельности класс в переменную
+    page.open()                                     # открытие страницы
+    page.should_be_login_link()                     # проверка присутствия на странице линки к логину
 

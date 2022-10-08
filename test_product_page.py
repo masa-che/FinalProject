6065f9911b7ad691813fc38d24cc71ee9cedbd16
masa-che <masache1983@gmail.com>
2022-10-08 18:52:41 +0300

    add to fix in test_product_page

--- test_product_page.py
+++ test_product_page.py
@@ -2,7 +2,7 @@ from .pages.product_page import ProductPage
 
 
 # черновик
-def test_guest_can_add_product_to_basket(browser, link):
+def test_guest_can_add_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
     product_page = ProductPage(browser, link)
     product_page.open()
